81343ad3a54dd5521c88f079a4e2d1adaff663ec
lenin <lennin_winner@hotmail.com>
2013-12-14 17:44:17 -0500

    olii

---
+++ js/Scriptfvf.js
@@ -0,0 +1,17 @@
+<ul class="nav pull-right">
+                            <li><a href="#">Inicio</a></li>
+                            <li class="divider-vertical"></li>
+                            <li><a href="#nos">Nosotros</a></li>
+                            <li class="divider-vertical"></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nuestros trabajos <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">this is test</a></li>
+                                    <li><a href="#">this is test</a></li>
+                                    <li><a href="#">this is test</a></li>
+                                    <li><a href="#">this is test</a></li>
+                                </ul>
+                            </li>
+                            <li class="divider-vertical"></li>
+                            <li><a href="#contact">Contactanos</a></li>
+                        </ul>
